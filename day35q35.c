5113c5724c4f964fb28cc7b326e25f7d6bc82e2c
Priyanshi0503 <priyanshi0057@gmail.com>
2026-03-20 23:16:01 +0530

    Create day35q35.c
    100 days of code

---
+++ day35q35.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define MAX 100
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+// Enqueue
+void enqueue(int x) {
+    if (rear == MAX - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+    if (front == -1) front = 0;
+    rear++;
+    queue[rear] = x;
+}
+
+// Display
+void display() {
+    if (front == -1) {
+        printf("Queue is empty\n");
+        return;
+    }
+    for (int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main() {
+    int n, x;
+    
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+    
+    display();
+    
+    return 0;
+}
