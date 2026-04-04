ecb435b55575d1ca4d92be3e84ce79a4b8d8dcb0
Priyanshi0503 <priyanshi0057@gmail.com>
2026-04-04 21:20:02 +0530

    Create q52day52.c

---
+++ q52day52.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0) return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = newNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2*i + 1;
+            int rightIndex = 2*i + 2;
+
+            if (leftIndex < n)
+                nodes[i]->left = nodes[leftIndex];
+            if (rightIndex < n)
+                nodes[i]->right = nodes[rightIndex];
+        }
+    }
+
+    return nodes[0]; // root
+}
+
+// LCA function
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    if (root == NULL)
+        return NULL;
+
+    // If either node matches
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* left = findLCA(root->left, n1, n2);
+    struct Node* right = findLCA(root->right, n1, n2);
+
+    // If both sides return non-null → LCA
+    if (left != NULL && right != NULL)
+        return root;
+
+    // Otherwise return non-null side
+    return (left != NULL) ? left : right;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* root = buildTree(arr, n);
+
+    struct Node* lca = findLCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("%d", lca->data);
+
+    return 0;
+}
