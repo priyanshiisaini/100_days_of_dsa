551d19a956950b7a99fb24f396c67aa5be27b96a
Priyanshi0503 <priyanshi0057@gmail.com>
2026-04-12 12:24:31 +0530

    Create q63day63.c

---
+++ q63day63.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(struct Node* adj[], int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    struct Node* newNode2 = createNode(u);
+    newNode2->next = adj[v];
+    adj[v] = newNode2; // remove for directed graph
+}
+
+void dfs(int v, struct Node* adj[], int visited[]) {
+    visited[v] = 1;
+    printf("%d ", v);
+
+    struct Node* temp = adj[v];
+    while (temp) {
+        if (!visited[temp->data]) {
+            dfs(temp->data, adj, visited);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    struct Node* adj[n];
+    int visited[n];
+
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(adj, u, v);
+    }
+
+    int s;
+    scanf("%d", &s);
+
+    dfs(s, adj, visited);
+
+    return 0;
+}
